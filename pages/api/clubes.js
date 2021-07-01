0198ea668871e2c1451b9be2380be2a4f7173125
Ana Carolina <paulohenrique@MacBook-Air-de-Paulo.local>
2021-06-30 23:36:53 -0300

    remove comments

--- pages/api/clubes.js
+++ pages/api/clubes.js
@@ -4,21 +4,13 @@ const { MongoClient } = require('mongodb');
 // Create cached connection variable
 let cachedDb = null;
 
-let clubes = null;
-
 async function connectToDatabase(uri) {
-
-  let ids = null;
 
   if (cachedDb) {
     return cachedDb;
   }
 
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  
-  //const db = client.db("base_team");
-
-  //const db = await client.db(url.parse(uri).pathname.substr(1));
 
   return client;
 }
@@ -28,7 +20,7 @@ module.exports = async (req, res) => {
 
   db.connect(err => {
     console.log("Connected to MongoDB server...");
-    const ids = db.db(process.env.MONGODB_DB).collection(process.env.MONGO_DB_COLLECTION) // substitute your database and collection names
+    const ids = db.db(process.env.MONGODB_DB).collection(process.env.MONGO_DB_COLLECTION)
     ids.find({}).toArray(function(err, result) {
         if(!err) {
           console.log("find query executed...")    
