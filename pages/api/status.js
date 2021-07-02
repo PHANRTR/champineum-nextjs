de36cf29a167980b6cf09b28a7a814fce05f7886
Paulo Henrique <ph.nunes3@gmail.com>
2021-07-02 14:40:38 -0300

    Remove comments

--- pages/api/status.js
+++ pages/api/status.js
@@ -2,10 +2,8 @@
 
   const response = await fetch('https://api.cartolafc.globo.com/mercado/status');
   const jsonResponse =  await response.json();
-  console.log(jsonResponse);
 
   res.json(jsonResponse);
-
 }
 
 export default status;
